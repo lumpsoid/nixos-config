348c9dbd079e568f8fa9f497d86ef925cac27233
lumpsoid <mmgeeki@gmail.com>
2024-08-16 02:28:23 +0200

    nixvim config update

--- host/omen/nixvim.nix
+++ host/omen/nixvim.nix
@@ -60,14 +60,153 @@
       };
     };
 
+    keymaps = [
+      {
+        mode = "n";
+        key = "-";
+        action = "<cmd>Oil<CR>";
+        options = {
+          desc = "open file manager oil";
+          silent = true;
+        };
+      }      
+      {
+        mode = "n";
+        key = "<leader>y";
+        action = ''"+yy'';
+        options = {
+          desc = "copy to the clipboard normal";
+          silent = true;
+        };
+      }      
+      {
+        mode = "v";
+        key = "<leader>y";
+        action = ''"+y'';
+        options = {
+          desc = "copy to the clipboard visual";
+          silent = true;
+        };
+      }      
+      {
+        mode = [ "n" "v" ];
+        key = "<leader>p";
+        action = ''"+p'';
+        options = {
+          desc = "past from the clipboard";
+          silent = true;
+        };
+      }      
+      {
+        mode = "n";
+        key = "<leader>sv";
+        action = "<cmd>vs<CR><c-w>w<CR>";
+        options = {
+          desc = "splite tab vertically and focus on it";
+          silent = true;
+        };
+      }      
+      {
+        mode = "n";
+        key = "<leader>sh";
+        action = "<cmd>sp<CR><c-w>w<CR>";
+        options = {
+          desc = "splite tab horizontally and focus on it";
+          silent = true;
+        };
+      }      
+    ];
+
+    files = {
+      "ftplugin/markdown.lua" = {
+        keymaps = [
+          {
+            mode = "n";
+            key = "<leader>gh";
+            action = "<cmd>e index.md<CR>";
+            options = {
+              desc = "go home";
+              silent = true;
+            };
+          }      
+        ];
+      };
+    };
+
     plugins = {
+
       bufferline = {
         enable = true;
       };
 
       nvim-autopairs.enable = true;
 
-      oil.enable = true;
+      indent-blankline.enable = true;
+
+      oil= {
+        enable = true;
+        settings = {
+          view_options = {
+            show_hidden = true;
+          };
+        };
+      };
+
+      mkdnflow = {
+        enable = true;
+        createDirs = false;
+        wrap = true;
+        perspective = {
+          priority = "root";
+          fallback = "current";
+          rootTell = "index.md";
+        };
+        links = {
+          style = "wiki";
+        };
+        mappings = {
+          MkdnNextLink = false;
+          MkdnPrevLink = false;
+          MkdnIncreaseHeading = false;
+          MkdnDecreaseHeading = false;
+          MkdnNewListItem = false;
+          MkdnNewListItemAboveInsert = false;
+          MkdnNewListItemBelowInsert = false;
+        };
+        modules = {
+          bib = false;
+        };
+        extraOptions = {
+          "name_is_source" = true;
+        };
+      };
+
+      fzf-lua = {
+        enable = true;
+        keymaps = {
+          "<leader>sf" = {
+            action = "files";
+            options = {
+              desc = "fzf search filenames";
+              silent = true;
+            };
+          };
+          "<leader>sb" = {
+            action = "buffers";
+            options = {
+              desc = "fzf search buffers";
+              silent = true;
+            };
+          };
+          "<leader>h" = {
+            action = "oldfiles";
+            options = {
+              desc = "open history pane";
+              silent = true;
+            };
+          };
+        };
+      };
 
       treesitter = {
         enable = true;
