999ff3d646d81c79f97945e5e7a08f0f5c1a0663
lumpsoid <mmgeeki@gmail.com>
2024-08-17 18:37:57 +0200

    updated layout for devshellnix script

---
+++ modules/scripts/devshellkeep/devshellkeep.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  options = {
+    scripts.devshellnix.enable =
+      lib.mkEnableOption "enable bulding 'devshellnix' script";
+  };
+
+  config = lib.mkIf config.scripts.devshellnix.enable {
+    home.packages = [
+      ( pkgs.writeShellScriptBin "devshellnix" (builtins.readFile ./devshellkeep.sh) )
+    ];
+  };
+}
