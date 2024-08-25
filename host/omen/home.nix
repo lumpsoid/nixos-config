2ec57eb28402b21a3b963a7872f1497478d23505
lumpsoid <mmgeeki@gmail.com>
2024-08-25 14:58:32 +0200

    pause with badwolf

--- host/omen/home.nix
+++ host/omen/home.nix
@@ -24,7 +24,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    badwolf
+    #badwolf
     bat
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
