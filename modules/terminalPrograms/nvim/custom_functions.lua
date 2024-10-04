991c1c6be8ec22cfe295beb3220daddec1eeb212
lumpsoid <mmgeeki@gmail.com>
2024-10-04 19:13:42 +0200

    refactor: presonal custom functions for nvim

--- modules/terminalPrograms/nvim/custom_functions.lua
+++ modules/terminalPrograms/nvim/custom_functions.lua
@@ -43,11 +43,18 @@ function M.zettelstamp()
   return os.date("%Y%m%d%H%M%S")
 end
 
-function M.linkwrap(text)
+function M.create_wiki_link(text)
   return '[[' .. text .. ']]'
 end
 
-function M.writeFile(path, text)
+---@param text string
+---@param link string
+---@return string
+function M.create_markdown_link(text, link)
+  return string.format("[%s](%s)", text, link)
+end
+
+function M.write_file(path, text)
   local filewrite = io.open(path, "w")
   if filewrite == nil then
     print(string.format('%s path is nil', path))
@@ -60,7 +67,7 @@ end
 ---comment
 ---@param line string
 ---@return string
-function M.cleanline(line)
+function M.clean_line(line)
   local lineNew = line:gsub(' *- ', '')
   lineNew = lineNew:gsub('%s*$', '')
   return lineNew
@@ -113,17 +120,27 @@ function M.delCurrentFile()
   end)
 end
 
-function M.currentLink()
+---comment
+---@param text string
+---@return nil
+local function setClipboard(text)
+  vim.fn.setreg('+', text)
+end
+
+function M.current_link()
   local path_to_file = vim.api.nvim_buf_get_name(0)
   local id = vim.fn.fnamemodify(path_to_file, ":t:r")
   -- Opens a file in read mode
   local file = io.open(path_to_file, "r")
+  if file == nil then
+    return ""
+  end
   -- prints the first line of the file
   local header = file:read()
   file:close()
 
-  local link = "[[" .. id .. "]] " .. M.cleanHeadline(header):lower()
-  vim.fn.setreg('+', link)
+  local link = M.create_wiki_link(id) .. " " .. M.cleanHeadline(header):lower()
+  setClipboard(link)
   return link
 end
 
@@ -133,7 +150,7 @@ function M.pathToCurrentFolder()
   return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
 end
 
-function M.createFilePath(fileName)
+function M.create_file_path(fileName)
   local system = jit.os
   local currentFolder = M.pathToCurrentFolder()
   if system == "Windows" then
@@ -143,22 +160,60 @@ function M.createFilePath(fileName)
   end
 end
 
+-- change it into switch statement
+-- and call different functions for 
+-- creating from selection
+-- from ui input
+-- maybe something else?
+function M.new_note()
+  local new_note_header
+  vim.ui.input(
+    { prompt = 'Enter new note header: ' },
+    function(input)
+      new_note_header = input
+    end
+  )
+  if new_note_header == '' then
+    return
+  end
+  if new_note_header == nil then
+    return
+  end
+
+  local parent_note = M.current_link()
+  local ztl = M.zettelstamp()
+  -- dynamicly take current folder to create note in it
+  local filename = ztl .. '.md'
+  local file_path = M.create_file_path(filename)
+  local new_note_text = '# ' .. new_note_header .. note_template .. parent_note
+  -- or you can use nvim.api.nvim_put() instead
+  M.write_file(file_path, new_note_text)
+
+  local new_note_link = M.create_markdown_link(new_note_header, './' .. filename)
+
+  M.textInsert(new_note_link)
+
+  -- local prefixLength = string.len(linePrefix)
+  -- vim.api.nvim_win_set_cursor(0, { pos[1], prefixLength + 2 })
+  -- mkdn.links.followLink()
+end
+
 function M.createID()
-  local main_note = M.currentLink()
+  local main_note = M.current_link()
   local ztl = M.zettelstamp()
   local pos = vim.api.nvim_win_get_cursor(0)
   local currentLine = vim.api.nvim_get_current_line()
   -- dynamicly take current folder to create note in it
-  local file_path = M.createFilePath(ztl .. ".md")
+  local file_path = M.create_file_path(ztl .. ".md")
   local new_header = vim.api.nvim_get_current_line()
-  new_header = M.cleanline(currentLine)
+  new_header = M.clean_line(currentLine)
   local text = '# ' .. new_header .. note_template .. main_note
   -- можно использовать для этого nvim.api.nvim_put()
-  M.writeFile(file_path, text)
+  M.write_file(file_path, text)
 
   -- write link to the line
   local linePrefix, lineText = string.match(currentLine, "^(%s*-%s)(.*)$")
-  local newLine = linePrefix .. M.linkwrap(ztl) .. " " .. lineText
+  local newLine = linePrefix .. M.create_wiki_link(ztl) .. " " .. lineText
   vim.api.nvim_set_current_line(newLine)
 
   local prefixLength = string.len(linePrefix)
@@ -201,11 +256,11 @@ function M.openJournal(shift)
   local date = M.dateData(shift)
   local formattedFileName = string.format("%d_%02d_%02d.md", date.year, date.month, date.day)
   local formattedHeader = string.format("%d %02d %02d %s", date.year, date.month, date.day, date.weekDay)
-  local filepath = M.createFilePath(formattedFileName)
+  local filepath = M.create_file_path(formattedFileName)
 
   if not M.fileExists(filepath) then
     local fileTemplate = "# " .. formattedHeader .. "\n#daily\n- "
-    M.writeFile(filepath, fileTemplate)
+    M.write_file(filepath, fileTemplate)
   end
 
   vim.api.nvim_command("edit " .. filepath)
@@ -231,11 +286,11 @@ function M.openJournalShift(shift)
   local formattedFileName = string.format("%d_%02d_%02d.md", previousDate.year, previousDate.month, previousDate.day)
   local formattedHeader = string.format("%d %02d %02d %s", previousDate.year, previousDate.month, previousDate.day,
     dayOfWeek)
-  local filepath = M.createFilePath(formattedFileName)
+  local filepath = M.create_file_path(formattedFileName)
 
   if not M.fileExists(filepath) then
     local fileTemplate = "# " .. formattedHeader .. "\n#daily\n- "
-    M.writeFile(filepath, fileTemplate)
+    M.write_file(filepath, fileTemplate)
   end
 
   vim.api.nvim_command("edit " .. filepath)
@@ -257,7 +312,7 @@ function M.openJournalSameDay()
     return
   end
 
-  local filepath = M.createFilePath(formattedFileName)
+  local filepath = M.create_file_path(formattedFileName)
   vim.api.nvim_command("edit " .. filepath)
 end
 
@@ -336,7 +391,7 @@ function M.indexTags()
   end
   local tags = M.collectTags()
   local tagString = table.concat(tags, "\n")
-  M.writeFile(filename, tagString)
+  M.write_file(filename, tagString)
 end
 
 return M
