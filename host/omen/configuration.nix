3335dac4adcfc719182ff645a85881d477ce1a81
lumpsoid <mmgeeki@gmail.com>
2024-11-07 12:51:31 +0100

    feat: added vscodium with extensions

--- host/omen/configuration.nix
+++ host/omen/configuration.nix
@@ -162,7 +162,21 @@
     shell = pkgs.fish;
     packages = with pkgs; [
       #  thunderbird
-      vscodium
+      (vscode-with-extensions.override (oldAttrs: {
+        vscode = vscodium.fhsWithPackages (ps: with ps; [elixir]);
+        vscodeExtensions = with vscode-extensions;
+          [
+            elixir-lsp.vscode-elixir-ls
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "remote-ssh-edit";
+              publisher = "ms-vscode-remote";
+              version = "0.47.2";
+              sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+            }
+          ];
+      }))
       android-udev-rules
       alejandra
       fossil
