093cb6762c04519accdf3cc6b4c427eb57a5391c
lumpsoid <mmgeeki@gmail.com>
2025-01-06 21:10:45 +0100

    refactor: removed stale rec on sessionVariables

--- host/omen/configuration.nix
+++ host/omen/configuration.nix
@@ -103,7 +103,7 @@
 
   environment = {
     #shells = with pkgs; [ zsh ];
-    sessionVariables = rec {
+    sessionVariables = {
       XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_DATA_HOME = "$HOME/.local/share";
